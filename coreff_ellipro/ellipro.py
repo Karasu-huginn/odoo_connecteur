d3854c89927ddf5c6332ba9c4a9430f29bcffe01
karasu-huginn <erwann.quin@supinfo.com>
2024-08-01 15:03:38 +0200

    update

--- coreff_ellipro/ellipro.py
+++ coreff_ellipro/ellipro.py
@@ -35,7 +35,9 @@ class Admin:
         ET.SubElement(client, "contractId").text = self.contract_id
         ET.SubElement(client, "userId").text = self.user_id
         ET.SubElement(client, "password").text = self.password
-        ET.SubElement(context, "appId", attrib={"version": "1"}).text = "WSRISK"
+        ET.SubElement(context, "appId", attrib={"version": "1"}).text = (
+            "WSRISK"
+        )
         ET.SubElement(context, "date").text = "2013-11-05T17:38:15+01:00"
 
 
@@ -67,11 +69,17 @@ class Search:
         ET.SubElement(address, "country", attrib={"code": "FRA"})
 
         search_options = ET.SubElement(request, "searchOptions")
-        ET.SubElement(search_options, "phoneticSearch").text = self.phonetic_search
+        ET.SubElement(search_options, "phoneticSearch").text = (
+            self.phonetic_search
+        )
         ET.SubElement(search_options, "maxHits").text = self.max_hits
         ET.SubElement(search_options, "mainOnly").text = self.main_only
-        ET.SubElement(search_options, "customContent").text = self.custom_content
-        ET.SubElement(search_options, "companyStatus").text = self.company_status
+        ET.SubElement(search_options, "customContent").text = (
+            self.custom_content
+        )
+        ET.SubElement(search_options, "companyStatus").text = (
+            self.company_status
+        )
         ET.SubElement(search_options, "establishmentStatus").text = (
             self.establishment_status
         )
@@ -88,14 +96,18 @@ class Order:
     def set_element(self, root):
         request = ET.SubElement(root, "request")
         ET.SubElement(request, "country", attrib={"code": "FRA"})
-        ET.SubElement(request, "id", attrib={"type": "src"}).text = self.company_id
+        ET.SubElement(request, "id", attrib={"type": "src"}).text = (
+            self.company_id
+        )
         ET.SubElement(
             request,
             "product",
             attrib={"range": self.product_id, "version": self.product_version},
         )
         delivery_options = ET.SubElement(request, "deliveryOptions")
-        ET.SubElement(delivery_options, "outputMethod").text = self.output_method
+        ET.SubElement(delivery_options, "outputMethod").text = (
+            self.output_method
+        )
         ET.SubElement(delivery_options, "format").text = self.format
 
 
@@ -107,11 +119,13 @@ class Catalogue:
     def set_element(self, root):
         request = ET.SubElement(root, "request")
         ET.SubElement(request, "country", attrib={"code": self.country_code})
-        ET.SubElement(request, "id", attrib={"type": "src"}).text = self.company_id
+        ET.SubElement(request, "id", attrib={"type": "src"}).text = (
+            self.company_id
+        )
 
 
-# * send a request to Ellipro and returns an elementTree object
 def search(admin, request, request_type, lang="FR", version="2.2"):
+    """Send XML request to Ellipro and return an elementTree object."""
     headers = {"Content-Type": "application/xml"}
     root = ET.Element(
         f"{request_type}Request", attrib={"lang": lang, "version": version}
@@ -128,8 +142,8 @@ def search(admin, request, request_type, lang="FR", version="2.2"):
     return ET.fromstring(request_result.text)
 
 
-# * create a list of suggestions based on a SvcSearch request response in a form of an elementTree object
 def search_response_handle(response):
+    """Parse a SvcSearch request response and return a list of dictionaries."""
     suggestions = []
     for establishment in response.iter("establishment"):
         suggestion = {}
@@ -137,22 +151,24 @@ def search_response_handle(response):
         suggestion["coreff_company_code"] = establishment.findall(
             "id[@idName='SIREN']"
         )[0].text
-        suggestion["ellipro_siren"] = establishment.findall("id[@idName='SIREN']")[
-            0
-        ].text
-        suggestion["ellipro_siret"] = establishment.findall("id[@idName='SIRET']")[
-            0
-        ].text
+        suggestion["ellipro_siren"] = establishment.findall(
+            "id[@idName='SIREN']"
+        )[0].text
+        suggestion["ellipro_siret"] = establishment.findall(
+            "id[@idName='SIRET']"
+        )[0].text
         suggestion["ellipro_identifiant_interne"] = establishment.findall(
             "id[@idName='Identifiant interne']"
         )[0].text
         if establishment.findall("communication[@type='phone']") != []:
-            suggestion["phone"] = establishment.findall("communication[@type='phone']")[
-                0
-            ].text
+            suggestion["phone"] = establishment.findall(
+                "communication[@type='phone']"
+            )[0].text
         suggestion["city"] = establishment.findall("address/cityName")[0].text
         suggestion["zip"] = establishment.findall("address/cityCode")[0].text
-        suggestion["street"] = establishment.findall("address/addressLine")[0].text
+        suggestion["street"] = establishment.findall("address/addressLine")[
+            0
+        ].text
         if establishment.findall("name[@type='businessname']") != []:
             suggestion["ellipro_business_name"] = establishment.findall(
                 "name[@type='businessname']"
@@ -161,10 +177,12 @@ def search_response_handle(response):
             suggestion["ellipro_trade_name"] = establishment.findall(
                 "name[@type='tradename']"
             )[0].text
-        suggestion["ellipro_city"] = establishment.findall("address/cityName")[0].text
-        suggestion["ellipro_zipcode"] = establishment.findall("address/cityCode")[
+        suggestion["ellipro_city"] = establishment.findall("address/cityName")[
             0
         ].text
+        suggestion["ellipro_zipcode"] = establishment.findall(
+            "address/cityCode"
+        )[0].text
         suggestion["ellipro_street_address"] = establishment.findall(
             "address/addressLine"
         )[0].text
@@ -177,15 +195,22 @@ def search_response_handle(response):
 
 
 def parse_order(order):
+    """Parse a SvcOnlineOrder request response and return a dictionary."""
     parsed_order = {}
     for response in order.findall("response"):
         name = response.findall("intlReport/header/report/reportId")[0].text
         price = (
-            response.findall("intlReport/header/report/defaultCurrencyUnit")[0].text
+            response.findall("intlReport/header/report/defaultCurrencyUnit")[
+                0
+            ].text
             + " "
-            + response.findall("intlReport/header/report/defaultCurrency")[0].text
+            + response.findall("intlReport/header/report/defaultCurrency")[
+                0
+            ].text
         )
-        parsed_order["ellipro_order_result"] = name + " pour le prix de " + price
+        parsed_order["ellipro_order_result"] = (
+            name + " pour le prix de " + price
+        )
         parsed_order["ellipro_rating_score"] = (
             int(
                 response.findall(
